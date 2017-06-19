8c5ab1ab8348777bd1b4e86c997fd504a95c10af
Hristo Tanev <hristotanev7@gmail.com>
2017-06-19 14:37:21 +0100

    Added a modal when choosing to add a new recipe.

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  addRecipe() {
+    console.log('adding a recipe...')
+  }
+
   render() {
     return (
-      <h1>Hi there!</h1>
+      <div class="container">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addRecipe">Add Recipe</button>
+        <div class="modal fade" id="addRecipe" role="dialog">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">Add Recipe</h4>
+              </div>
+              <div class="modal-body">
+                <label><strong>Recipe name</strong></label>
+                <input type="text" class="form-control" />
+                <br />
+                <label><strong>Recipe ingredients</strong></label>
+                <textarea class="form-control" rows="5"></textarea>
+              </div>
+              <div class="modal-footer">
+                <button onClick={this.addRecipe.bind(this)} type="button" class="btn btn-primary" data-dismiss="modal">Add Recipe</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
